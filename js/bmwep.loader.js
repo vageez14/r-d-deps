331e424fd562268f66ab61f12a8d218160aba647
Angelo <angelo.vagenas1@bellmedia.ca>
2024-06-19 13:01:47 -0400

    commit

--- js/bmwep.loader.js
+++ js/bmwep.loader.js
@@ -1,1 +1,16 @@
-console.log("FROM LOADER", window);
+const scriptSrc = new URL(document.currentScript.src);
+const scriptSrcPathName = scriptSrc.pathname;
+
+function getPath(scriptSrcPathName) {
+    const scriptTags = Array.from(document.getElementsByTagName("script"));
+    const loaderSrc = scriptTags.find(tag => tag.src.includes(scriptSrcPathName))?.src;
+
+    if (loaderSrc) {
+        const url = new URL(loaderSrc);
+        return `${url.protocol}//${url.host}`;
+    } else {
+        return undefined; // or handle the case where loaderSrc is not found
+    }
+}
+
+console.log(getPath(scriptSrcPathName));
