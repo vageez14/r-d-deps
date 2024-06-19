26b5c4b375bb7b7a18455399fd78d1758f09ab91
Angelo <angelo.vagenas1@bellmedia.ca>
2024-06-19 14:59:36 -0400

    adding more

--- js/bmwep.loader.js
+++ js/bmwep.loader.js
@@ -7,14 +7,27 @@
         .filter(src => src.includes(scriptSrcPathName))
         .reduce((loaderSrc, currentSrc) => currentSrc ? new URL(currentSrc).origin : undefined, undefined);
 
-    const deps = [
+    const cssdeps = [
+        "https://cdn.jsdelivr.net/npm/reset-css@latest/reset.min.css",
+    ];
+
+    cssdeps.forEach(dep => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = dep;
+        link.onload = () => console.log(`Loaded: ${dep}`);
+        link.onerror = () => console.error(`Failed to load: ${dep}`);
+        document.head.appendChild(link);
+    });
+
+    const jsdeps = [
         "https://cdn.jsdelivr.net/npm/swiper@latest/swiper-element-bundle.min.js",
         "https://test.bmwep.bellmedia.ca/common/large-poster-slider.js",
         "https://test.bmwep.bellmedia.ca/common/poster-shelf.js",
         `${basePath}/js/user-info.js`,
     ];
 
-    deps.forEach(dep => {
+    jsdeps.forEach(dep => {
         const script = document.createElement("script");
         script.src = dep;
         script.onload = () => console.log(`Loaded: ${dep}`);
@@ -23,4 +36,133 @@
     });
 })();
 
+MEDIAQUERYSMALL = `@media (max-width:992px)`;
+MEDIAQUERYMEDIUM = `@media (min-width:993px)`;
+MEDIAQUERYLARGE = `@media (min-width:1919px)`;
+THEME = {
+    breakpoints : {
+        values : {
+            xsmall : "0px",
+            small : "575px",
+            medium : `993px`,
+            large : `1500px`,
+            xlarge : `1920px`,
+        },
+    },
+    typography : {
+        title1 : {
+            [MEDIAQUERYSMALL] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "32px",
+                lineHeight : "40px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYLARGE] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "64px",
+                lineHeight : "78px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYMEDIUM] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "56px",
+                lineHeight : "70px",
+                fontWeight : "700",
+            }
+        },
+        title4 : {
+            [MEDIAQUERYSMALL] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "18px",
+                lineHeight : "22px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYLARGE] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "30px",
+                lineHeight : "36px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYMEDIUM] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "28px",
+                lineHeight : "36px",
+                fontWeight : "700",
+            }
+        },
+        largetitle2 : {
+            [MEDIAQUERYSMALL] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "42px",
+                lineHeight : "52px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYLARGE] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "72px",
+                lineHeight : "90px",
+                fontWeight : "700",
+            },
+            [MEDIAQUERYMEDIUM] : {
+                fontFamily : "Proxima Nova Bold",
+                fontSize : "64px",
+                lineHeight : "76px",
+                fontWeight : "700",
+            },
+        },
+        body : {
+            [MEDIAQUERYSMALL] : {
+                fontFamily : "Proxima Nova",
+                fontSize : "14px",
+                lineHeight : "18px",
+                fontWeight : "400",
+            },
+            [MEDIAQUERYLARGE] : {
+                fontFamily : "Proxima Nova",
+                fontSize : "18px",
+                lineHeight : "24px",
+                fontWeight : "400",
+            },
+            [MEDIAQUERYMEDIUM] : {
+                fontFamily : "Proxima Nova",
+                fontSize : "16px",
+                lineHeight : "24px",
+                fontWeight : "400",
+            }
+        },
+    },
+    components : {
+        MuiCard : {
+            styleOverrides : {
+                root : {
+                    background : "transparent",
+                },
+            },
+        },
+        MuiPaper : {
+            defaultProps : {
+                elevation : 0,
+                square : true,
+            },
+        },
+        MuiSkeleton : {
+            styleOverrides : {
+                root : {
+                    backgroundColor : "#27282e",
+                    borderRadius : "4px",
+                }
+            }
+        },
+        MuiTypography : {
+            styleOverrides : {
+                root : {
+                    color : "#ffffff",
+                    textTransform : "none",
+                    textDecoration : "none",
+                    letterSpacing : "0%",
+                },
+            },
+        },
+    },
+};
 
