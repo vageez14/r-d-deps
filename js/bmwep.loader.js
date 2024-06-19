b8b058670721a1f5de430e78f53abd20faa3480b
Angelo <angelo.vagenas1@bellmedia.ca>
2024-06-19 14:23:14 -0400

    updating loader

--- js/bmwep.loader.js
+++ js/bmwep.loader.js
@@ -1,26 +1,26 @@
-const scriptSrc = new URL(document.currentScript.src);
-const scriptSrcPathName = scriptSrc.pathname;
-const scriptTags = Array.from(document.getElementsByTagName("script")) ?? [];
-const basePath = scriptTags
-    .map(tag => tag.src)
-    .filter(src => src.includes(scriptSrcPathName))
-    .reduce((loaderSrc, currentSrc) => currentSrc ? new URL(currentSrc).origin : undefined, undefined);
+(() => {
+    const scriptSrc = new URL(document.currentScript.src);
+    const scriptSrcPathName = scriptSrc.pathname;
+    const scriptTags = Array.from(document.getElementsByTagName("script")) ?? [];
+    const basePath = scriptTags
+        .map(tag => tag.src)
+        .filter(src => src.includes(scriptSrcPathName))
+        .reduce((loaderSrc, currentSrc) => currentSrc ? new URL(currentSrc).origin : undefined, undefined);
 
-console.log("basePath", basePath);
-
-const loadDeps = async() => {
     const deps = [
-        "https://cdn.jsdelivr.net/npm/swiper@latest/swiper-element-bundle.min.js"
-            `${basePath}/js/user-info.js`,
+        "https://cdn.jsdelivr.net/npm/swiper@latest/swiper-element-bundle.min.js",
+        "https://test.bmwep.bellmedia.ca/common/large-poster-slider.js",
+        "https://test.bmwep.bellmedia.ca/common/poster-shelf.js",
+        `${basePath}/js/user-info.js`,
     ];
 
-    await Promise.all(deps.map(dep => new Promise((resolve, reject) => {
+    deps.forEach(dep => {
         const script = document.createElement("script");
         script.src = dep;
-        script.onload = resolve;
-        script.onerror = reject;
+        script.onload = () => console.log(`Loaded: ${dep}`);
+        script.onerror = () => console.error(`Failed to load: ${dep}`);
         document.head.appendChild(script);
-    })));
-};
+    });
+})();
 
-await loadDeps();
+
